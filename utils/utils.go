a5f87a3d8d124900bd92775cf1380cee32569014
kaweendras <salitha.wijerathna@gmail.com>
2024-08-14 14:56:06 +0530

    CHG | Utils

---
+++ utils/utils.go
@@ -0,0 +1,23 @@
+// utils/utils.go
+package utils
+
+import (
+	"fmt"
+	"os"
+)
+
+// GetABI reads the ABI file and returns its content.
+func GetABI() ([]byte, error) {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get current working directory: %v", err)
+	}
+	fmt.Println("Current working directory:", cwd)
+
+	abiFile, err := os.ReadFile("../ABI/bamla.json")
+	if err != nil {
+		return nil, fmt.Errorf("failed to read ABI file: %v", err)
+	}
+
+	return abiFile, nil
+}
